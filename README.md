a82fa6709b0fcba00816783f9246dba6579a0c33
gomatf <125886829+gomatf@users.noreply.github.com>
2023-03-02 21:46:53 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-typescript-20230302-ramda
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-k5bmv2)
---
+++ index.ts
@@ -0,0 +1,16 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+// Ramda を試す
+import * as R from 'ramda';
+console.log('Test message');
+
+const obj = {
+  message: 'Hello, Ramda!',
+};
+
+console.log(R.clone(obj));
