6bdd78b711bcf44f1387917c57822e6961a0d3bd
gomatf <125886829+gomatf@users.noreply.github.com>
2023-03-13 16:59:40 +0900

    Ramda examples

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # stackblitz-typescript-20230302-ramda
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-k5bmv2)
+
+## 参考
+
+- [Ramda Documentation](https://ramdajs.com/)
--- index.ts
+++ index.ts
@@ -2,15 +2,76 @@
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const appDiv: HTMLElement | null = document.getElementById('app');
+if (appDiv) {
+  appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+}
+
+// ================
+// Preparation
+
+// データ型の定義
+type Menu = {
+  name: string;
+  price: number;
+};
 
 // Ramda をインポート
 import * as R from 'ramda';
-console.log('Test message');
 
-const obj = {
-  message: 'Hello, Ramda!',
+// ================
+// Object
+
+// オブジェクトのディープコピー
+const menu: Menu = {
+  name: 'オムライス',
+  price: 780,
 };
+console.log(R.clone(menu));
 
-console.log(R.clone(obj));
+// オブジェクトの走査
+console.log(R.keys(menu)); // キーを配列に変換
+console.log(R.values(menu)); // 値を配列に変換
+
+for (const [key, value] of R.toPairs(menu)) {
+  // キーと値
+  console.log(`${key}: ${value}`);
+}
+
+// ================
+// Relation
+
+// 等価判定（プリミティブでもオブジェクトでも可）
+console.log(R.equals(menu, { name: 'オムライス', price: 780 })); // true
+console.log(R.equals(null, null)); // true
+
+// 重複を取り除く
+const arr1 = [1, 2, 3, 4, 5, 2, 4];
+console.log(R.uniq(arr1));
+
+const arr2 = [
+  { name: 'タコライス', price: 680 },
+  { name: 'オムライス', price: 780 },
+  { name: 'カレーライス', price: 820 },
+  { name: 'オムライス', price: 780 },
+  { name: 'オムライス', price: 830 },
+];
+console.log(R.uniq(arr2));
+
+// オブジェクトの特定のプロパティで重複を判定する
+const eqByMenuName = R.eqBy(R.prop('name'));
+console.log(R.uniqWith(eqByMenuName)(arr2));
+
+// 集合の差分（重複も取り除かれる）
+const arr3 = [4, 7, 9, 1];
+console.log(R.difference(arr1, arr3));
+
+// ================
+// Logic
+
+// デフォルト値
+const defaultToMenu = R.defaultTo<Menu>(menu);
+console.log(defaultToMenu(null));
+console.log(defaultToMenu(undefined));
+console.log(defaultToMenu(13));
+console.log(defaultToMenu({ name: 'カレーライス', price: 720 }));
